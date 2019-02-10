a0d2ef6e83c16489d6b125548bbb7d22093f0c9f
josevitali <jvitali@itba.edu.ar>
2019-02-10 12:45:10 -0300

    add DFS

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,18 @@
 module Main where
 
 import Lib
+import Problems.Board
+import Engine.DFS
+
+import Data.Maybe
 
 main :: IO ()
-main = someFunc
+main = do { let board = Problems.Board.newBoard [['.','.','.', 'p'],
+                                                ['.','.','w','.'],
+                                                ['.','w','.','w'],
+                                                ['.','.','g','.']]
+        --   ; putStrLn (show board) }
+          -- ; putStrLn (show $ dfsSearch board isGoal validMoves) }
+          ; let search = fromJust (dfsSearch board isGoal validMoves)
+          ; printBoards $ snd (search)
+          ; putStrLn (show (fst search)) }
---
+++ src/Engine/DFS.hs
@@ -0,0 +1,36 @@
+module Engine.DFS
+(dfsSearch)
+where
+
+import Data.Stack as Stack
+
+import Data.Maybe
+
+data Node a = Node { state :: a
+                   , parent :: Maybe (Node a)
+                   , cost :: Int
+                   , depth :: Int
+                   }
+
+dfsSearch :: (Eq a) => a -> (a -> Bool) -> (a -> [(a, Int)]) -> Maybe (Int, [a])
+dfsSearch startState isGoal nextStates
+    | isGoal startState = Just (0, [startState])
+    | otherwise = dfs isGoal nextStates (stackPush stackNew startNode) []
+        where startNode = Node {state=startState, parent=Nothing, cost=0, depth=0}
+
+dfs :: (Eq a) => (a -> Bool) -> (a -> [(a, Int)]) -> Stack (Node a) -> [a] -> Maybe (Int, [a])
+dfs isGoal nextStates stack visited
+    | stackIsEmpty stack = Nothing
+    | isGoal nodeState = Just (nodeToList currentNode)
+    | elem nodeState visited = dfs isGoal nextStates popedStack visited
+    | otherwise = dfs isGoal nextStates nextStack (nodeState:visited)
+        where Just (popedStack, Node {state=nodeState, parent=nodeParent, cost=nodeCost, depth=nodeDepth}) = stackPop stack
+              currentNode = Node {state=nodeState, parent=nodeParent, cost=nodeCost, depth=nodeDepth}
+              childStates = nextStates nodeState
+              nextNodeList = map (\child -> Node {state=fst child, parent=Just currentNode, cost= snd child, depth=nodeDepth + 1 }) childStates
+              nextStack = foldr (\node currStack -> stackPush currStack node) popedStack nextNodeList
+
+nodeToList :: Node a -> (Int, [a])
+nodeToList Node {state=nodeState, parent=Nothing, cost=nodeCost, depth=_} = (nodeCost, [nodeState])
+nodeToList Node {state=nodeState, parent=Just nodeParent, cost=nodeCost, depth=_} = ((fst ret) + nodeCost, (snd ret) ++ [nodeState])
+                                                                                where ret = nodeToList nodeParent
